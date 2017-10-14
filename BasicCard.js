724b74e92e2dfa24f0a3818b68475ca0593f941d
Michael <michael@Michaels-MBP.fios-router.home>
2017-10-13 20:11:51 -0500

    flashcard

---
+++ BasicCard.js
@@ -0,0 +1,20 @@
+exports.BasicCard = function(front, back) {
+	this.front = front;
+	this.back = back;
+}
+
+
+exports.ClozeCard = function(text, cloze) {
+	
+	var textToLower = text.toLowerCase();
+	var clozeToLower = cloze.toLowerCase();
+
+	
+	if (!textToLower.includes(clozeToLower)) {
+		console.log('ERROR: cloze-deletion does not appear within full text -- <' + cloze + '>');
+		return;
+	}
+
+	this.full = text;
+	this.cloze = cloze;
+	this.partial = text.replace(cloze, '...');
